c20de05a1b0238ce1af1a07b4ae4daaa437aa173
Andrianov <andrianov.vital@yandex.ru>
2022-12-03 00:37:47 +0300

    #5 1. Downloading selected format and sending it to user

--- src/Sound_Cloud_Bot/SCDownloader.py
+++ src/Sound_Cloud_Bot/SCDownloader.py
@@ -22,27 +22,27 @@ class SoundCloud:
         with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
             self.info = ydl.extract_info(url, download=False)
 
-        # Скачивание выбранного формата
-        def download(self, track_format):
-            i = 0
-            for element in self.info['formats']:
-                if element['format_id'] == track_format:
-                    break
-                else:
-                    i += 1
+    # Скачивание выбранного формата
+    def download(self, track_format):
+        i = 0
+        for element in self.info['formats']:
+            if element['format_id'] == track_format:
+                break
+            else:
+                i += 1
 
-            response = requests.get(url=self.info['formats'][i]['url'], stream=False,
-                                    headers=self.info['formats'][i]['http_headers'])
+        response = requests.get(url=self.info['formats'][i]['url'], stream=False,
+                                headers=self.info['formats'][i]['http_headers'])
 
-            file_name = StringBuilder()
-            for element in self.info['title']:
-                if element in self.escape_symbols:
-                    file_name.append('_')
-                else:
-                    file_name.append(element)
+        file_name = StringBuilder()
+        for element in self.info['title']:
+            if element in self.escape_symbols:
+                file_name.append('_')
+            else:
+                file_name.append(element)
 
-            with open(f"{file_name.to_string()}.mp3", 'wb') as f:
-                for chunk in response.iter_content(chunk_size=8192):
-                    f.write(chunk)
+        with open(f"{file_name.to_string()}.mp3", 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
 
-            return f'{file_name.to_string()}.mp3'
+        return f'{file_name.to_string()}.mp3'
